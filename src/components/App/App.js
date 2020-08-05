1319d3b3e8e730418baccf85dff7135683339af5
Liudmila_Arzamasova <liudmila.arzamasova@gmail.com>
2020-08-05 15:27:41 +0300

    feat: render header block

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
-
 import 'bootswatch/dist/slate/bootstrap.min.css';
 
+import Header from '../Header/Header';
+
 const App = () => {
-  return <div className='App'></div>;
+  return (
+    <div>
+      <Header />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import { speciesBirds } from '../../helpers/birds';
+
+import cls from './Header.module.css';
+
+const Header = () => {
+  return (
+    <div className='container'>
+      <div className={cls.TopPanel}>
+        <a className={cls.Logo} title='Songbird' href='/'>
+          <h1 className={cls.LogoTitle}>
+            <span className={cls.LogoTitleStart}>Song</span>
+            <span className={cls.LogoTitleEnd}>bird</span>
+          </h1>
+        </a>
+        <span className={cls.Score}>Score: 0</span>
+      </div>
+      <ul className={cls.Nav}>
+        {speciesBirds.map(({ id, kind }) => {
+          return (
+            <li className={cls.NavItem} key={id}>
+              <span className={cls.NavLink}>{kind}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Header;
